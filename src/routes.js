e57cdcfdeafe27681e5eebdec983e4c3232e8066
EmesDev <contato.emersonfranco@gmail.com>
2021-03-30 19:19:47 -0300

    api- and-front

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,14 @@ const UserController = require("./controllers/UserController");
 
 const routes = express.Router();
 
+routes.use(express.static(__dirname + '/public/css'));
+
 routes.post("/users", UserController.store);
+
+routes.get('/', (req, res) => {
+  res.sendFile(__dirname +  "/views/form.html");
+
+});
 
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,13 @@ const routes = require("./routes");
 const app = express();
 const port = 3333;
 
+const bodyParser = require('body-parser');
+
 require("./database");
+
+//body-parser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(routes);
